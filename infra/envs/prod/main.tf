cce4e8084aca69e011d99468c5876faeb1b3b9ba
Dan Campbell <dan.campbell412@gmail.com>
2025-12-19 08:19:23 -0500

    "Corrected formatting"

--- infra/envs/prod/main.tf
+++ infra/envs/prod/main.tf
@@ -46,15 +46,15 @@ resource "azurerm_container_registry" "acr" {
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "kv" {
-  name                      = "kv-${local.name_prefix}"
-  location                  = azurerm_resource_group.rg.location
-  resource_group_name       = azurerm_resource_group.rg.name
-  tenant_id                 = data.azurerm_client_config.current.tenant_id
-  sku_name                  = "standard"
-  enable_rbac_authorization = true # or rbac_authorization_enabled depending on your provider
+  name                       = "kv-${local.name_prefix}"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = "standard"
+  enable_rbac_authorization  = true # or rbac_authorization_enabled depending on your provider
   soft_delete_retention_days = 7
   purge_protection_enabled   = false
-  tags                      = local.tags
+  tags                       = local.tags
 
   network_acls {
     default_action = "Deny"
