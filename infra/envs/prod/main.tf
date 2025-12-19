707e52e4d0a1bfd4742b85d9f88a8d7e02c98591
Dan Campbell <dan.campbell412@gmail.com>
2025-12-19 08:15:46 -0500

    "Added security to main.tf"

--- infra/envs/prod/main.tf
+++ infra/envs/prod/main.tf
@@ -46,16 +46,22 @@ resource "azurerm_container_registry" "acr" {
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "kv" {
-  name                       = "kv-${local.name_prefix}"
-  location                   = azurerm_resource_group.rg.location
-  resource_group_name        = azurerm_resource_group.rg.name
-  tenant_id                  = data.azurerm_client_config.current.tenant_id
-  sku_name                   = "standard"
-  enable_rbac_authorization  = true
+  name                      = "kv-${local.name_prefix}"
+  location                  = azurerm_resource_group.rg.location
+  resource_group_name       = azurerm_resource_group.rg.name
+  tenant_id                 = data.azurerm_client_config.current.tenant_id
+  sku_name                  = "standard"
+  enable_rbac_authorization = true # or rbac_authorization_enabled depending on your provider
   soft_delete_retention_days = 7
   purge_protection_enabled   = false
-  tags                       = local.tags
+  tags                      = local.tags
+
+  network_acls {
+    default_action = "Deny"
+    bypass         = "AzureServices"
+  }
 }
+
 
 # Give the deploying identity (whoever runs terraform) permissions to manage secrets in this KV.
 # In GitHub Actions, this will be the OIDC-authenticated principal.
